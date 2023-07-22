d76143e952be4d1ae16b01dc51778c59175d19d5
jeremyben-meir <jsb459@cornell.edu>
2023-07-22 14:57:12 -0700

    add amplify with storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "lakefront9a1c8e0f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "lakefrontstorage": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
