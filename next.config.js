13a308a96238e1198feb89f304bccff9a3d220e5
jeremyben-meir <jsb459@cornell.edu>
2023-07-22 18:27:47 -0700

    return to old npm with npm i in amplify setup

--- next.config.js
+++ next.config.js
@@ -5,20 +5,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  // webpack: (config, { isServer }) => {
-  //   // Fixes npm packages that depend on `fs` module
-  //   if (!isServer) {
-  //     config.resolve.fallback = {
-  //       ...config.resolve.fallback,
-  //       "aws-crt": false,
-  //     }
-  //   }
-
-  //   return config
-  // },
-  // experimental: {
-  //   serverActions: true,
-  // },
+  experimental: {
+    serverActions: true,
+  },
 }
 
 module.exports = nextConfig
